3bfed9c9edf76f5b1deeb55e5b971313b5323e73
Mario Armellini <mario.armellini@opendeusto.es>
2025-11-11 19:41:00 +0100

    Initial commit: Spring Boot project with Gradle

---
+++ src/main/java/com/ecoembes/e9/EcoembesE9Application.java
@@ -0,0 +1,13 @@
+package com.ecoembes.e9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoembesE9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoembesE9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecoembes/e9/EcoembesE9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoembes.e9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoembesE9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
